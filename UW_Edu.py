9ee28a8a20241700ba0e140ed28a17375648f416
ShrinivasKR <1amthebes!>
2015-04-23 13:23:03 -0700

    Printing out UCB ischool and UW education

--- UW_Edu.py
+++ UW_Edu.py
@@ -9,11 +9,11 @@ soup = BeautifulSoup(html)
 faculty = soup.findAll('td', attrs={'class': 'views-field views-field-user'})
 for td in faculty:
     facultyURL = ("https://education.uw.edu" + td.find('a')['href'])
-    print(facultyURL)
+    #print(facultyURL)
     with urllib.request.urlopen(facultyURL) as response:
         html = response.read()
         facultySoup = BeautifulSoup(html)
-        education = facultySoup.findAll('div', attrs={'class' : 'field-field-person-education'})
-        for res in education:
-            print(res.text)
+        print(facultySoup.title.text)
+        for link in soup.find_all(id="education"):
+            print(facultySoup.p.text)
 
