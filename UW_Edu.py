68ee7fdd97290aabb324c21248bb9e1b99b88cdf
ShrinivasKR <1amthebes!>
2015-04-23 13:24:28 -0700

    added comments

--- UW_Edu.py
+++ UW_Edu.py
@@ -3,16 +3,19 @@ __author__ = 'Shrinivas'
 from bs4 import BeautifulSoup
 import urllib.request
 
+#initial access to main page
 with urllib.request.urlopen('https://education.uw.edu/people') as response:
     html = response.read()
 soup = BeautifulSoup(html)
 faculty = soup.findAll('td', attrs={'class': 'views-field views-field-user'})
+#looping through each individual faculty
 for td in faculty:
     facultyURL = ("https://education.uw.edu" + td.find('a')['href'])
     #print(facultyURL)
     with urllib.request.urlopen(facultyURL) as response:
         html = response.read()
         facultySoup = BeautifulSoup(html)
+        #printing out name and department and college
         print(facultySoup.title.text)
         for link in soup.find_all(id="education"):
             print(facultySoup.p.text)
